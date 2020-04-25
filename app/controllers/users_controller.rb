a256a5a8b0decd1682df7619e92faedf81b4e05a
KodamaHiroaki <kdm@KodamaHiroakinoMacBook-Air.local>
2020-04-25 19:28:20 +0900

    FinishBookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,33 @@
 class UsersController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :correct_user, only: [:edit]
+
   def index
   	@book = Book.new
   	@users = User.all
-    @user = User.find(params[:id])
+    @user = current_user
+    @users = User.all
   end
 
   def show
-  	@book = Book.new
-  	@books = Book.all
-    @user = User.find(params[:id])
+  	@user = User.find(params[:id])
+    @users = User.all
+    @book = Book.new
+    @books = @user.books
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
   end
 
   def edit
@@ -24,8 +36,25 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    user.update(user_params)
-    redirect_to user_path
+    if @user.update(user_params)
+      flash[:notice] = "successfully"
+      redirect_to user_path(@user.id)
+    else
+      flash[:notice] = "error"
+      render :edit
+    end
   end
 
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def correct_user
+    user = User.find(params[:id])
+    if user != current_user
+      redirect_to user_path(current_user)
+      flash[:notice] = "error"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 	devise_for :users
-	resources :books, only: [:new, :index, :create, :edit, :show]
+	root 'homes#top'
+	get 'home/about' => 'homes#about'
+
+	resources :books, only: [:new, :index, :create, :edit, :show, :destroy, :update]
 	resources :users, only: [:edit, :index, :show, :update]
-  root 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
